4c91f99a705f4d87090690866666bc5f157a1208
Haswell <shuyangf@student.unimelb.edu.au>
2020-09-25 23:17:51 +1000

    Add prefix to url path

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const cors = require('cors');
-
+const prefix = "/api/"
 app.use(cors());
 // use the body-parser middleware, which parses request bodies into req.body
 // support parsing of json
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // GET home page
-app.get("/", (req, res) => {
+app.get(prefix, (req, res) => {
   res.send("<H1>app</H1>");
 });
 
@@ -27,11 +27,12 @@ const loginRouter = require("./routes/loginRouter");
 const signupRouter = require("./routes/signupRouter");
 const userRouter = require("./routes/userRouter");
 
-app.use("/findCaregiver", findCaregiverRouter);
-app.use("/findPatient", findPatientRouter);
-app.use("/login", loginRouter);
-app.use("/signup", signupRouter);
-app.use("/user", userRouter);
+
+app.use(prefix + "findCaregiver", findCaregiverRouter);
+app.use(prefix + "findPatient", findPatientRouter);
+app.use(prefix + "login", loginRouter);
+app.use(prefix + "signup", signupRouter);
+app.use(prefix + "user", userRouter);
 
 
 // start app and listen for incoming requests on port 3000
